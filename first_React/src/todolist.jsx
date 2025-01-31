c491554467ebf8a5b74a99ab85db745284a2cd07
Hugo0O3 <hugomancini2.0@gmail.com>
2025-01-31 12:00:48 +0100

    ok

--- first_React/src/todolist.jsx
+++ first_React/src/todolist.jsx
@@ -6,12 +6,13 @@ function Todolist() {
             <h1>My Todo App</h1>
             <form action="post">
                 <input type="text" id="todo-input" name="todo" placeholder="Type a new todo" />
+                <br />
                 <button type="submit">Add Todo</button>
             </form>
             <h2>Todos</h2>
-            <input type="text" /><label for="learn">Learn React!</label>
-            <input type="text" /><label for="awesome"> Be awesome!</label>
-            <input type="text" /><label for="fun"> Have fun les pioupious 🐤!</label>
+            <input type="checkbox" /><label for="learn"> Learn React!</label><br />
+            <input type="checkbox" /><label for="awesome"> Be awesome!</label><br />
+            <input type="checkbox" /><label for="fun"> Have fun les pioupious 🐤!</label>
         </>
     )
 }
